693e6d1ac2ac27f37c213510289e33b385b39b00
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-10-21 17:51:07 +0530

    Update hollowRectangle.cpp

--- PatternPrints/hollowRectangle.cpp
+++ PatternPrints/hollowRectangle.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
-
 void hollowRectangle(int rows, int cols)
 {
-    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
 int main()
 {
