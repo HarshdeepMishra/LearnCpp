aca53ea28fb7ba2374ded8c2bc22fd5d76d81c11
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-10-22 12:39:00 +0530

    Create rightAngledTriangle.cpp

---
+++ PatternPrints/rightAngledTriangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void rightAngledTriangle(int n)
+{
+    for (int i = n; i <= 1; i--)
+    {
+        for (int j = 1; j <= i; j++ ){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+    rightAngledTriangle(n);
+
+}
