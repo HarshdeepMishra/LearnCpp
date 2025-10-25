0718b2cc15ef38b190ce6df308d4e02c838c9b41
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-10-25 12:45:40 +0530

    Fix loop condition in rightAngledTriangle function

--- PatternPrints/rightAngledTriangle.cpp
+++ PatternPrints/rightAngledTriangle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void rightAngledTriangle(int n)
 {
-    for (int i = n; i <= 1; i--)
+    for (int i = n; i >= 1; i--)
     {
         for (int j = 1; j <= i; j++ ){
             cout << "*";
