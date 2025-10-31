d610d564e25495b4010bc2869c1ec48ab537f58d
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2025-10-31 13:28:50 +0530

    Update halfDiamondPattern.cpp

--- PatternPrints/halfDiamondPattern.cpp
+++ PatternPrints/halfDiamondPattern.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void halfDiamondPattern()
+void halfDiamondPattern(int n)
 {
-    
+    for (int i = 1; i<n; i++)
+    {
+        for(int j = 1; j<n; j++)
+        {
+            
+        }
+    }
 }
 int main()
 {
-
+    int n;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+    halfDiamondPattern(n);
 }
